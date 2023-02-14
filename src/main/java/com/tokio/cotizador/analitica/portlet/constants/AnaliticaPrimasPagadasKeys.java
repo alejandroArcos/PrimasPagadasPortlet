5327173b4ed1320f8e6d355dd044ad87a67c1c4b
alejandroArcos <alejandro.arcos@globalquark.com.mx>
2023-02-14 12:35:05 -0600

    First commit
    First commit

---
+++ src/main/java/com/tokio/cotizador/analitica/portlet/constants/AnaliticaPrimasPagadasKeys.java
@@ -0,0 +1,7 @@
+package com.tokio.cotizador.analitica.portlet.constants;
+
+public class AnaliticaPrimasPagadasKeys {
+
+	public static final String PORTLET_NAME = "com_tokio_corizador_analitica_portlet_portlet_PrimaspagadasportletmvcportletPortlet";
+	
+}
